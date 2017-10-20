3b7f02a2acd405c12eaeb2e8ab07299cfabec88e
gongchen <gongchen@usc.edu>
2017-10-20 15:35:00 -0700

    routing works

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,6 @@
+<template>
+<div class="">
+  Dashboard Component
+
+</div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './components/App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import Dashboard from './components/Dashboard.vue'
+import Signin from './components/Signin.vue'
+
+const router = new VueRouter({
+  mode: 'history', //history mode makes sure that when new url hit, change happens on current page
+  routes: [
+    { path: '/dashboard', component: Dashboard },
+    { path: '/signin', component: Signin }
+  ]
+})
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
