162376e48dbb6d689dc02c41c77e319d1b800787
gongchen <gongchen@usc.edu>
2017-10-20 16:49:51 -0700

    simple sign in page

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -1,5 +1,33 @@
 <template>
-<div class="">
-  Sign In Component
+<div class="from-inline">
+  <h3>
+    Sign In
+  </h3>
+  <div class="form-group">
+    <input
+      type="text"
+      placeholder="email"
+      class="form-control"
+      v-model="email"
+    />
+    <input
+      type="password"
+      placeholder="password"
+      class="form-control"
+      v-model="password"
+    >
+    <button class="btn btn-primary">Sign in</button>
+  </div>
 </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
