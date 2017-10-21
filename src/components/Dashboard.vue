033d5ba7d0a1117a9961f7edb022456918e20375
gongchen <gongchen@usc.edu>
2017-10-20 20:53:30 -0700

    other events show up at page in raw form

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -5,13 +5,13 @@
   <hr>
   <AddEvent />
   <hr>
-  {{$store.state}}
+  {{$store.state.events}}
 </div>
 </template>
 
 
 <script>
-import { firebaseApp } from '../firebaseApp'
+import { firebaseApp, eventsRef } from '../firebaseApp'
 import AddEvent from './AddEvent.vue'
 export default {
   methods: {
@@ -22,6 +22,15 @@ export default {
   },
   components: {
     AddEvent
+  },
+  mounted() {
+    eventsRef.on('value', snap => {
+      let events = []
+      snap.forEach(event => {
+        events.push(event.val())
+      })
+      this.$store.dispatch('setEvents', events)//Automatically update
+    })
   }
 }
 </script>
